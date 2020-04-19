de205d1236bb13d50a4c01774184326eceea8d38
Ys2025 <1829749816@qq.com>
2020-04-19 16:47:43 +0800

    七牛云免费存储空间搭建图床

--- src/main/java/cn/yanghuisen/qiniu/utils/QiNiuContent.java
+++ src/main/java/cn/yanghuisen/qiniu/utils/QiNiuContent.java
@@ -7,7 +7,7 @@ package cn.yanghuisen.qiniu.utils;
  * @Description TODO
  */
 public class QiNiuContent {
-    public static String accessKey = "0PQdGcm6RWGb_4rfYtuFWVjbd5Y9guJdZ7dhrSQF" ;
-    public static String secretKey ="cBzrusqvep5ItybXrcvb1QmoyRDHv1n7YyjLmpfH" ;
-    public static String bucket = "a2025-imgs";
+    public static String accessKey = "accessKey" ;
+    public static String secretKey ="secretKey" ;
+    public static String bucket = "空间名称";
 }
